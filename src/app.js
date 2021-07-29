c3047c64739d3408d4f07a5bac4c9dabe30b17ee
Thanh hau <Thanhhau9979@gmail.com>
2021-07-29 18:38:46 +0700

    done

--- src/app.js
+++ src/app.js
@@ -12,6 +12,8 @@ let slide_next = hero_slide.querySelector('.slide-next')
 
 let slide_prev = hero_slide.querySelector('.slide-prev')
 
+let header = document.querySelector('header')
+
 showSlide = (index) => {
     hero_slide.querySelector('.slide.active').classList.remove('active')
     hero_slide.querySelector('.slide-control-item.active').classList.remove('active')
@@ -50,3 +52,36 @@ setInterval(() => {
     if (!hero_slide_play) return 
     nextSlide()
 }, 3000);
+
+// change header style when scroll
+window.addEventListener('scroll', () => {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
+        header.classList.add('shrink')
+    } else {
+        header.classList.remove('shrink')
+    }
+    
+
+})
+// element show on scroll
+let scroll = window.requestAnimationFrame || function(callback) {window.setTimeout(callback, 1000/60)}
+
+let el_to_show = document.querySelectorAll('.show-on-scroll')
+
+isElInViewPort = (el) => {
+    let rect = el.getBoundingClientRect()
+
+    let distance = 200
+
+    return (rect.top <= (window.innerHeight - distance || document.documentElement.clientHeight - distance))
+}
+
+loop = () => {
+    el_to_show.forEach(el => {
+        if (isElInViewPort(el)) el.classList.add('show')
+    })
+
+    scroll(loop)
+}
+
+loop()
